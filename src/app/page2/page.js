de1941f0ae38032d5eefa2d49790e5d1c60c4173
Atipat Krongsin <atipat.k@arise.tech>
2024-09-12 11:29:01 +0700

    remove check process.env

--- src/app/page2/page.js
+++ src/app/page2/page.js
@@ -1,8 +1,9 @@
 import { client } from '../../lib/serverAppInsight';
 export default async function Page2() {
-  if(process.env.NODE_ENV === 'production'){
+
+  // if(process.env.NODE_ENV === 'production'){
     client.trackEvent({name: `my custom event on ${process.env.NODE_ENV}` , properties: {customProperty: "custom property value"}});
-  }
+  // }
 
   return (
     <div/>
